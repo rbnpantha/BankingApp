89de1f6d1778dbb054804004ff61fcea20cdc653
Nilam Dahal <Nilam Dahal>
2016-12-18 16:06:21 -0600

    created domain

--- src/main/java/edu/cs544/eafinal/domain/User.java
+++ src/main/java/edu/cs544/eafinal/domain/User.java
@@ -1,5 +1,41 @@
 package edu.cs544.eafinal.domain;
 
 public class User {
+	private String id;
+	private String firstName;
+	private String lastName;
+	private Address address;
+	private UserCredential userCredential;
 
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	public UserCredential getUserCredential() {
+		return userCredential;
+	}
+	public void setUserCredential(UserCredential userCredential) {
+		this.userCredential = userCredential;
+	}
+	
 }
---
+++ src/main/java/edu/cs544/eafinal/domain/UserCredential.java
@@ -0,0 +1,35 @@
+package edu.cs544.eafinal.domain;
+
+public class UserCredential {
+
+	String username;
+	String password;
+	String verifyPassword;
+	Boolean enabled;
+
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getVerifyPassword() {
+		return verifyPassword;
+	}
+	public void setVerifyPassword(String verifyPassword) {
+		this.verifyPassword = verifyPassword;
+	}
+	public Boolean getEnabled() {
+		return enabled;
+	}
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;
+	}
+
+}
